40f57a7ecfc4e6488ea509435309965281c5e9aa
Conor Barr <conorlbarr@gmail.com>
2021-04-03 20:32:51 -0400

    ModelBase now implements IDisposable

--- PrismBase/Mvvm/ModelBase.cs
+++ PrismBase/Mvvm/ModelBase.cs
@@ -1,9 +1,17 @@
-﻿namespace PrismBase.Mvvm
+﻿using System;
+
+namespace PrismBase.Mvvm
 {
-    public abstract class ModelBase : DataErrorBindableBase
+    public abstract class ModelBase : DataErrorBindableBase, IDisposable
     {
+        protected bool mIsDisposed;
+
         public ModelBase() : base()
         {
         }
+
+        public void Dispose() => Dispose(true);
+
+        protected abstract void Dispose(bool disposing);
     }
 }
