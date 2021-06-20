955e3280f296ca48a07c636c9b0eacf3a187d490
Conor Barr <conorlbarr@gmail.com>
2021-06-20 15:41:55 -0400

    Initial commit of extension classes

---
+++ MvvmBase/Extensions/EnumExtension.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace MvvmBase.Extensions
+{
+    public static class EnumExtension
+    {
+        /// <summary>
+        /// Gets the description that is stored in a <see cref="DescriptionAttribute"/> for an enum value.
+        /// </summary>
+        /// <remarks>An <see cref="Exception"/> is thrown if the enum value does not have a <see cref="DescriptionAttribute"/> and if the 
+        /// <paramref name="value"/> does not exist in the enum.</remarks>
+        /// <typeparam name="T">The type of <see cref="Enum"/>.</typeparam>
+        /// <param name="value">The enum value.</param>
+        /// <returns><see cref="DescriptionAttribute.Description"/></returns>
+        public static string GetDescription<T>(this T value) where T : Enum
+        {
+            return value.GetDescription(value.GetType());
+        }
+
+
+        /// <summary>
+        /// Gets the description that is stored in a <see cref="DescriptionAttribute"/> for an enum value.
+        /// </summary>
+        /// <remarks>An <see cref="Exception"/> is thrown if the enum value does not have a <see cref="DescriptionAttribute"/> and if the 
+        /// <paramref name="value"/> does not exist in the enum.</remarks>
+        /// <typeparam name="T">The type of <see cref="Enum"/>.</typeparam>
+        /// <param name="value">The enum value.</param>
+        /// <returns><see cref="DescriptionAttribute.Description"/></returns>
+        public static string GetDescription(this Enum value, Type enumType)
+        {
+            // Get the name of enum value and then use it to get value's attributes/metadata
+            string fieldName = Enum.GetName(enumType, value);
+            if (fieldName == null)
+            {
+                throw new Exception($"The value '{value}' does not exist in the '{enumType.Name}' enum.");
+            }
+            FieldInfo field = enumType.GetField(fieldName); // Get the fields attributes/metadata
+
+            // Get the description attribute and return its description
+            DescriptionAttribute descAttribute = field.GetCustomAttribute<DescriptionAttribute>();
+            if (descAttribute == null)
+            {
+                throw new Exception($"The property '{fieldName}' from the enum '{enumType}' does not have a {nameof(DescriptionAttribute)}.");
+            }
+            return descAttribute.Description;
+        }
+    }
+}
---
+++ MvvmBase/Extensions/TypeExtension.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace MvvmBase.Extensions
+{
+    public enum DescriptionType
+    {
+        Field = 1 << 0,
+        Property = 1 << 1,
+        FieldAndProperty = 1 << 2
+    }
+
+    public static class TypeExtension
+    {
+        /// <summary>
+        /// Gets descriptions for all fields and/or properties for a <see cref="Type"/>. Add a 
+        /// <see cref="DescriptionAttribute(string)"/> to a property or field to give it a description.
+        /// </summary>
+        /// <param name="type">The type of object that has the <see cref="DescriptionAttribute"/> assigned to
+        /// its fields and properties.</param>
+        /// <param name="descriptionType">Used to specify whether you want this method to get descriptions from 
+        /// fields, properties, or fields and properties.</param>
+        /// <returns>The descriptions that were retrieved for the <see cref="Type"/>.</returns>
+        public static string[] GetDescriptions(this Type type, DescriptionType descriptionType)
+        {
+            List<string> descriptions = new List<string>();
+            FieldInfo[] fields = null;
+            PropertyInfo[] properties = null;
+
+            // Get descriptions for all fields
+            if (descriptionType == DescriptionType.Field || descriptionType == DescriptionType.FieldAndProperty)
+            {
+                fields = type.GetFields();
+
+                for (int i = 0; i < fields.Length; i++)
+                {
+                    DescriptionAttribute descAttribute = fields[i].GetCustomAttribute<DescriptionAttribute>();
+                    if (descAttribute != null)
+                    {
+                        descriptions.Add(descAttribute.Description);
+                    }
+                }
+            }
+
+            // Get descriptions for all properties
+            if (descriptionType == DescriptionType.Property || descriptionType == DescriptionType.FieldAndProperty)
+            {
+                properties = type.GetProperties();
+                for (int i = 0; i < properties.Length; i++)
+                {
+                    DescriptionAttribute descAttribute = properties[i].GetCustomAttribute<DescriptionAttribute>();
+                    if (descAttribute != null)
+                    {
+                        descriptions.Add(descAttribute.Description);
+                    }
+                }
+            }
+
+            return descriptions.ToArray();
+        }
+
+    }
+}
