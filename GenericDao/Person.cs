5676393456a4d003ffe1ef9662cf07d81feb2a98
Conor Barr <cbarr@conestogac.on.ca>
2022-01-03 14:29:41 -0500

    Remove test files

--- GenericDao/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GenericDao
-{
-    public class Person
-    {
-        public int? id { get; set; }
-        public string name { get; set; }
-        public int age { get; set; }
-    }
-}
